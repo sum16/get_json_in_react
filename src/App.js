3319c3d89a1f3c7043efe6cef4624a839cbd8a9f
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-23 18:48:31 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <button>users</button>
+        <button>id=1のユーザー</button>
+      </div>
+    </>
+  );
+}
